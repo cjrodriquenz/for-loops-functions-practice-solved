f8264ce058306e5a682126f963aadae29d7d8e0c
Cameron Rodriquenz <cjrodriquenz@gmail.com>
2025-03-04 10:33:57 -0500

    completed exercise 11

--- exercises/e11.js
+++ exercises/e11.js
@@ -6,7 +6,17 @@
 
 export function getAllWithdrawals(array) {
   // Your code goes here...
-
+  const withdrawalsSums = [];
+  for (let i = 0; i < array.length; i++) {
+    let sum = 0;
+    if (array[i].withdrawals) {
+      for (let j = 0; j < array[i].withdrawals.length; j++) {
+        sum += array[i].withdrawals[j];
+      }
+    }
+    withdrawalsSums.push(sum);
+  }
+  return withdrawalsSums;
 }
 
 // === TEST YOURSELF ===
